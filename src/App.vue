5298f80fa7ce7202dbf78a2729c886273f179d89
SoeSandarWin472 <soesandarwin472000@gmail.com>
2023-08-15 11:34:03 +0630

    mini game

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>How fast can u catch me?</h1>
+ <button @click="start" :disabled="isPlaying">play</button>
+ <div v-if="isPlaying">
+ <Block :delay="delay" @endGame="endGame"/>
+ </div>
+ <div v-if="end">
+   <Result :score="score"/>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "./components/Block.vue";
+import Result from "./components/Result.vue"
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block,
+    Result
+  },
+  data(){
+    return{
+      isPlaying:false,
+      delay:null,
+      score:0,
+      end:false
+
+    }
+  },
+  methods:{
+    start(){
+      this.isPlaying=true,
+      this.delay=2000+Math.random()*5000
+    },
+    endGame(score){
+      this.score=score;
+      this.isPlaying=false;
+      this.end=true
+    }
   }
 }
 </script>
---
+++ src/components/Result.vue
@@ -0,0 +1,23 @@
+<template>
+   <p>Result is {{score}}ms</p>
+   <div v-if="score<250">
+        <p>U r too fast</p>
+   </div>
+   <div v-else-if="score>250 && score <500">
+        <p>U r Normal</p>
+   </div>
+   <div v-else>
+        <p>U r too slow! try again</p>
+   </div>
+</template>
+
+<script>
+export default {
+    name:"ResultB",
+    props:["score"]
+}
+</script>
+
+<style>
+
+</style>
